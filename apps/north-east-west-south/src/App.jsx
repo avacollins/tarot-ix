daab855e0c9f19d43c09a7b807386145437234fe
Ava Collins <info@avacollins.dev>
2025-05-09 18:09:04 -0400

    chore: typescript

--- apps/north-east-west-south/src/App.jsx
+++ apps/north-east-west-south/src/App.jsx
@@ -1,20 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
-  return (
-    <View style={styles.container}>
+    return (<View style={styles.container}>
       <Text>Open up App.tsx to start working on your news app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+      <StatusBar style="auto"/>
+    </View>);
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
--- apps/tarot/App.tsx
+++ apps/tarot/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your news app</Text>
+      <Text>Open up App.tsx to start working on your tarot app!</Text>
       <StatusBar style="auto" />
     </View>
   );
